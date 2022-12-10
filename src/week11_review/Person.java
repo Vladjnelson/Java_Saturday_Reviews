51099fb9440e7c0a4e402676648eb7abe20e1a74
Vladimir Nelson <vladjnelson@gmail.con>
2022-12-10 11:38:00 -0500

    week 11 Person

---
+++ src/week11_review/Person.java
@@ -0,0 +1,54 @@
+package week11_review;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private char gender;
+
+    public Person(String name, int age, char gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.isEmpty() || name == null){
+            System.err.println("Name can not be null or empty");
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age<= 0){
+            System.err.println("Invalid Age: " + age);
+            System.exit(1);
+        }
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+}
